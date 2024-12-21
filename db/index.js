0c0ac20f9a2f6bf71805d388b6198fac266dd1aa
rafay <abdulrafayazad123@gmail.com>
2024-12-21 11:30:12 +0500

    backend connection string change only

--- db/index.js
+++ db/index.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const connectDb = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://umarBhai:umarBhai@cluster0.ulq1q.mongodb.net/basesbackend?retryWrites=true&w=majority&appName=Cluster0"
+      "mongodb+srv://abdulrafay:abdulrafay@cluster0.iqhhq.mongodb.net/NewDataBase4?retryWrites=true&w=majority&appName=Cluster0"
+      // "mongodb+srv://umarBhai:umarBhai@cluster0.ulq1q.mongodb.net/basesbackend?retryWrites=true&w=majority&appName=Cluster0"
     );
     console.log("MongoDB connected successfully to NewDatabaseName");
   } catch (error) {
